1d04b8b3dfee296877e4bcff8ef9504157fc9f7f
hiroshiyoshii <100889533+hiroshiyoshii@users.noreply.github.com>
2022-04-04 23:24:26 +0900

    HumanクラスにString型変数 hobbyを宣言

--- app/src/main/java/jp/techacademy/hiroshi/yoshii/kotlinlog/Human.kt
+++ app/src/main/java/jp/techacademy/hiroshi/yoshii/kotlinlog/Human.kt
@@ -4,8 +4,13 @@ import android.util.Log
 
 open class Human: Animal, Thinkable {
 
-     constructor(name: String, age: Int, hobby: String): super(name, age, hobby) {
-     }
+
+    var hobby: String
+
+    constructor(name: String, age: Int, hobby: String ): super(name, age) {
+
+     this.hobby = hobby
+    }
 
      override fun say() {
        Log.d("kotlintest", this.name + "(" + this.age + "歳)" )
